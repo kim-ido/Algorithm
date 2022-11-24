1e64a74774a5e1a8b743ca93b8df0fa03a1a9376
Kim Ido <117932171+kim-ido@users.noreply.github.com>
2022-11-24 20:05:11 +0900

    [Bronze II] Title: 블랙잭, Time: 80 ms, Memory: 11596 KB -BaekjoonHub

---
+++ 백준/Bronze/2798. 블랙잭/블랙잭.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int temp = 0;
+        int[] arr = new int[n];
+
+        st = new StringTokenizer(br.readLine(), " ");
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                for (int k = j + 1; k < n; k++) {
+                    int sum = arr[i] + arr[j] + arr[k];
+
+                    if (temp < sum && sum <= m)
+                        temp = sum;
+                }
+            }
+        }
+        System.out.println(temp);
+    }
+}
