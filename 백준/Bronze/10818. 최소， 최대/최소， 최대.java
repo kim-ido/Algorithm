b59f3b86f2f99e1badd81dd22413fcac2d65ae16
Kim Ido <117932171+kim-ido@users.noreply.github.com>
2022-11-26 15:16:33 +0900

    [Bronze III] Title: 최소, 최대, Time: 468 ms, Memory: 107180 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.java
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -1,22 +1,26 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
+        Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
-        int index = 0;
-        int[] arr = new int[N];
+        int max = -1000001;
+        int min = 1000001;
+
         while (st.hasMoreTokens()) {
-            arr[index] = Integer.parseInt(st.nextToken());
-            index++;
+            int val = Integer.parseInt(st.nextToken());
+            if (val > max) {
+                max = val;
+            }
+            if (val < min) {
+                min = val;
+            }
         }
-        Arrays.sort(arr);
-        System.out.println(arr[0] + " " + arr[N - 1]);
+        System.out.println(min + " " + max);
     }
 }
