ee688e59141b9476f9cdd300c7465c390f5d6554
Kim Ido <kimido987@gmail.com>
2022-12-06 00:31:03 +0900

    [Bronze III] Title: 최댓값, Time: 76 ms, Memory: 11488 KB -BaekjoonHub

--- 백준/Bronze/2562. 최댓값/최댓값.java
+++ 백준/Bronze/2562. 최댓값/최댓값.java
@@ -5,22 +5,16 @@ import java.io.InputStreamReader;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        int arr[] = new int[9];
-
-        for (int i = 0; i < 9; i++) {
-            arr[i] = Integer.parseInt(br.readLine());
-        }
-
+        
         int max = 0;
         int index = 0;
-        int count = 0;
 
-        for (int value : arr) {
-            count++;
-            if (value > max) {
-                max = value;
-                index = count;
+        for (int i = 0; i < 9; i++) {
+            int val = Integer.parseInt(br.readLine());
+
+            if (val > max) {
+                max = val;
+                index = i + 1;
             }
         }
         System.out.println(max);
