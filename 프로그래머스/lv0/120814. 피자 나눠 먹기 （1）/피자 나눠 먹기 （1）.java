0a70008ccbf97bd0284ecc6d514940ed5bc34b9a
Kim Ido <kimido987@gmail.com>
2022-12-06 15:41:24 +0900

    [level 0] Title: 피자 나눠 먹기 (1), Time: 0.02 ms, Memory: 72.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.java
+++ 프로그래머스/lv0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.java
@@ -1,14 +1,6 @@
 class Solution {
     public int solution(int n) {
-        int answer = 0;
-        int a = n/7;
-        int b = n%7;
-        
-        if(b == 0) {
-            answer = a * 1;
-        } else {
-            answer = a * 1 + 1;
-        }
+        int answer = (n%7 == 0) ? n/7 : n/7 + 1;
         return answer;
     }
 }
