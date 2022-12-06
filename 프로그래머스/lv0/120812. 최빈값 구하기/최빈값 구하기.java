ee40c9fdec229c3d0c78ff0f48d80db337cd3614
Kim Ido <kimido987@gmail.com>
2022-12-06 13:43:53 +0900

    [level 0] Title: 최빈값 구하기, Time: 0.06 ms, Memory: 76.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120812. 최빈값 구하기/최빈값 구하기.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int solution(int[] array) {
+        int answer = 0;
+        int arr[] = new int[1001];
+        int max = 0;
+        int maxIndex = 0;
+        int cnt = 0;
+        
+        for (int i = 0; i < array.length; i++) {
+            arr[array[i]]++;
+        }
+        
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+                maxIndex = i;
+            }
+        }
+        
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == max) cnt++;
+        }
+        
+        if (cnt > 1) return -1;
+        answer = maxIndex;
+        return answer;
+    }
+}
