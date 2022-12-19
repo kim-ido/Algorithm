e7d60a74626037e88b3d8e45ce5f4308239934fc
Kim Ido <kimido987@gmail.com>
2022-12-19 22:22:59 +0900

    [Silver IV] Title: 보물, Time: 80 ms, Memory: 11748 KB -BaekjoonHub

---
+++ 백준/Silver/1026. 보물/보물.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine());
+        int[] A = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            A[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(A);
+
+        Integer[] B = new Integer[N];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            B[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(B, Comparator.reverseOrder()); // 내림차순 정렬
+
+        int answer = 0;
+        for (int i = 0; i < N; i++) {
+            answer += A[i] * B[i];
+        }
+
+        bw.write(answer + "\n");
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
