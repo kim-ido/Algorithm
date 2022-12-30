7679c9128b15aa76889c0eb63c0d658376733218
Kim Ido <kimido987@gmail.com>
2022-12-30 22:27:52 +0900

    [Silver II] Title: 차이를 최대로, Time: 88 ms, Memory: 11924 KB -BaekjoonHub

--- 백준/Silver/10819. 차이를 최대로/차이를 최대로.java
+++ 백준/Silver/10819. 차이를 최대로/차이를 최대로.java
@@ -1,40 +1,41 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    boolean[] v;
-    int[] arr;
-    int answer = 0;
-    int n;
 
-    private void dfs(int cnt, int bf, int sum) {
+    public static int n;
+    public static int answer = 0;
+    public static int[] arr;
+    public static boolean[] visit;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+        arr = new int[n];
+        visit = new boolean[n];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        dfs(0, 0, 0);
+        System.out.println(answer);
+    }
+
+    public static void dfs(int cnt, int bf, int sum) {
         if (cnt == n) {
             answer = Math.max(answer, sum);
             return;
         }
 
         for (int i = 0; i < n; i++) {
-            if (v[i]) continue;
-            v[i] = true;
-            dfs(cnt+1, arr[i], cnt==0 ? 0:sum+Math.abs(bf-arr[i]));
-            v[i] = false;
+            if (visit[i]) {
+                continue;
+            }
+            visit[i] = true;
+            dfs(cnt + 1, arr[i], cnt == 0 ? 0 : sum + Math.abs(bf - arr[i]));
+            visit[i] = false;
         }
-    }
-
-    private void solution() throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        n = Integer.parseInt(br.readLine());
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        arr = new int[n];
-        v = new boolean[n];
-        for (int i = 0; i < n; i++) arr[i] = Integer.parseInt(st.nextToken());
-
-        dfs(0, 0, 0);
-        System.out.println(answer);
-    }
-
-    public static void main(String[] args) throws Exception {
-        new Main().solution();
     }
 }
