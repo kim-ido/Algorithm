5f79b89e8978402ee0dafd463acaeccda36843b6
Kim Ido 김이도 <kimido987@gmail.com>
2023-01-04 21:31:19 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 76 ms, Memory: 11456 KB -BaekjoonHub

--- 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.java
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.java
@@ -1,24 +1,27 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.IOException;
- 
+import java.io.*;
+
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		int N = Integer.parseInt(br.readLine());
-		
-		
-		long sum = 1;
-		long f1 = 0;
-		long f2 = 1;
-		
-		for(int i = 1 ; i < N ; i++) {
-			sum = f1 + f2;
-			f1 = f2;
-			f2 = sum;
-		}
-		
-		System.out.println(sum);
-	}
+    
+    static long[] arr;
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        arr = new long[n + 1];
+
+        for (int i = 0; i < n + 1; i++) {
+            arr[i] = -1;
+        }
+        
+        arr[0] = 0;
+        arr[1] = 1;
+        System.out.println(Fib(n));
+    }
+    
+    public static long Fib(int n) {
+        if (arr[n] == -1) {
+            arr[n] = Fib(n - 1) + Fib(n - 2);
+        }
+        return arr[n];
+    }
 }
