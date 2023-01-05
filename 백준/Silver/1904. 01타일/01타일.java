cbaf6c7b96a164334c2f65152abbb7de53736f3c
김이도 Kim Ido <kimido987@gmail.com>
2023-01-05 22:42:24 +0900

    [Silver III] Title: 01타일, Time: 88 ms, Memory: 11488 KB -BaekjoonHub

--- 백준/Silver/1904. 01타일/01타일.java
+++ 백준/Silver/1904. 01타일/01타일.java
@@ -1,29 +1,36 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-
+import java.io.IOException;
+ 
 public class Main {
+ 
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
 
-    public static int[] dp = new int[1000001];
-
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-
-        dp[0] = 0;
-        dp[1] = 1;
-        dp[2] = 2;
-
-        for (int i = 3; i < dp.length; i++) {
-            dp[i] = -1;
-        }
-
-        System.out.println(Tile(n));
-    }
-
-    public static int Tile(int n) {
-        if (dp[n] == -1) {
-            dp[n] = (Tile(n - 1) + Tile((n - 2))) % 15746;
-        }
-        return dp[n];
-    }
+		System.out.println(Tile(n));
+	}
+ 
+	public static int Tile(int a) {
+ 
+		if (a == 1) {
+			return 1;
+		} 
+ 
+		if (a == 2) {
+			return 2;
+		}
+ 
+		int val1 = 1;
+		int val2 = 2;
+		int sum = 0;
+ 
+		for (int i = 2; i < a; i++) {
+			sum = (val2 + val1) % 15746;
+			val1 = val2;
+			val2 = sum; 
+		}
+        
+		return sum;
+	}
 }
